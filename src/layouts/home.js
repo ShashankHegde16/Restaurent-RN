14d35e0d458e5fe9ccc9d023378839f7f5ea15f6
Shashank Hegde <shashank.hegde@neutrinos.co>
2020-12-10 13:58:04 +0530

    flatlist training

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 
 const Home = () => {
+    const array = [{
+        'name': 'Shashank'
+    }, { 'name': 'Hegde' }]
     return (
         <View style={styleSheet.container}>
             <Text style={{ fontSize: 20 }}>Greetings!</Text>
+            <FlatList
+                keyExtractor={(item) => item.name}
+                data={array}
+                renderItem={({ item }) => {
+                    return <Text>{item.name}</Text>
+                }}
+            />
         </View>);
 };
 
